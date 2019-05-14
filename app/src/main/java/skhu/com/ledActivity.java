e04964204df0b7c1f3877f1ddea704fb71974cc4
supermoon <enyo68@gmail.com>
2019-05-14 11:44:02 +0900

    5/14

--- app/src/main/java/skhu/com/ledActivity.java
+++ app/src/main/java/skhu/com/ledActivity.java
@@ -1,8 +1,7 @@
 package skhu.com;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,25 +9,26 @@ import android.content.Intent;
 import android.net.Uri;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View.OnClickListener;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import android.widget.ImageButton;
 
+public class ledActivity extends MainActivity {
 
-public class    MainActivity extends AppCompatActivity {
-    public static String MQTTHOST = "tcp://192.168.0.2:1883";
-    static String USERNAME = "root";
-    static String PASSWORD = "1234";
-    String topicstr = "iot/led1";
-    MqttAndroidClient client;
+    public String flag = "off";
+//    ImageButton btn_change;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_led);
+//        btn_change = (ImageButton) findViewById(R.id.led_living_btn);
+//        btn_change.setOnClickListener(this);
 
         String clientId = MqttClient.generateClientId();
         client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
@@ -40,50 +40,59 @@ public class    MainActivity extends AppCompatActivity {
         try {
             IMqttToken token = client.connect(options);
             token.setActionCallback(new IMqttActionListener() {
-                    @Override
+                @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    Toast.makeText(MainActivity.this, "connected!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ledActivity.this, "connected!", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Toast.makeText(MainActivity.this, "fail!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ledActivity.this, "fail!", Toast.LENGTH_SHORT).show();
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
+
     }
-    public void pub(View v){
+
+
+    public void pub_led1(View v) {
         String topic = topicstr;
-        String message = "ON";
+        String message = "";
+        if (flag == "off") {
+            flag = "on";
+            message = "ON";
+//            btn_change.setSelected(true);
+        } else if (flag == "on") {
+            flag = "off";
+            message = "OFF";
+//            btn_change.setSelected(false);
+        }
         try {
             client.publish(topic, message.getBytes(), 0, false);
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_camera) {
-//            Toast.makeText(this, "메뉴 클릭", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.12:625/videostream.cgi?user=admin&pwd=123456789"));
-            //웹페이지 들어가기
-            startActivity(intent);
+    public void pub_led2(View v) {
+        String topic = "iot/led2";
+        String message = "";
+        if (flag == "off") {
+            flag = "on";
 
-            return true;
+            message = "ON";
+        } else if (flag == "on") {
+            flag = "off";
+            message = "OFF";
         }
-        return super.onOptionsItemSelected(item);
+        try {
+            client.publish(topic, message.getBytes(), 0, false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void ledActivity(View view) {
-        Intent intent2 = new Intent(MainActivity.this, ledActivity.class);
-        startActivity(intent2);
-    }
+
 }
